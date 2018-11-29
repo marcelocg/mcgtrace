57b59e2addc2f213ab9cdc32b685b3e3d223e9e8
Marcelo <marcelocg@gmail.com>
2018-11-29 01:13:27 -0300

    Implement point addition

--- mcgtrace/point.py
+++ mcgtrace/point.py
@@ -1,5 +1,7 @@
-class Point:
+import mcgtrace.vector
 
+
+class Point:
     def __init__(self, *tup):
         self.x, self.y, self.z = tup
         self.w = 1.0
@@ -14,3 +16,24 @@ class Point:
 
     def to_tuple(self):
         return (self.x, self.y, self.z, self.w)
+
+    def add(self, other):
+        if isinstance(other, mcgtrace.vector.Vector):
+            return Point(self.x + other.x,
+                         self.y + other.y,
+                         self.z + other.z)
+        elif isinstance(other, tuple) and len(other) == 4:
+            if other[3] == 0:
+                return (self.x + other[0],
+                        self.y + other[1],
+                        self.z + other[2],
+                        self.w + other[3])
+            else:
+                raise ValueError(
+                    "Tuple passed as argument does not represent a Vector.")
+        elif isinstance(other, Point):
+            raise TypeError(
+                "Adding two points makes no sense. Try adding a Vector instead.")
+        else:
+            raise TypeError(
+                "Value is not a Vector or a 4-dimensional tuple")
--- tests/test_point.py
+++ tests/test_point.py
@@ -1,4 +1,6 @@
+import pytest
 from mcgtrace.point import Point
+from mcgtrace.vector import Vector
 
 
 class TestPoint(object):
@@ -29,3 +31,52 @@ class TestPoint(object):
     def test_point_as_tuple(self):
         point = Point(1, 1, 1)
         assert point.to_tuple() == (1.0, 1.0, 1.0, 1.0)
+
+    def test_addition_to_vector_object(self):
+        point = Point(3, -2, 5)
+        vector = Vector(-2, 3, 1)
+        destination = point.add(vector)
+        # a point added to a vector results in a translated point (w=1)
+        assert destination.to_tuple() == (1, 1, 6, 1)
+
+    def test_vector_addition_results_in_new_point_object(self):
+        starting_point = Point(3, -2, 5)
+        vector = Vector(-2, 3, 1)
+        destination = starting_point.add(vector)
+        assert isinstance(destination, Point)
+        assert destination is not starting_point
+
+    def test_addition_to_tuple_as_vector(self):
+        point = Point(3, -2, 5)
+        vector = (-2, 3, 1, 0)
+        destination = point.add(vector)
+        # a point added to a vector results in a translated point (w=1)
+        assert destination == (1, 1, 6, 1)
+
+    def test_tuple_as_vector_addition_results_in_new_tuple(self):
+        vector = (3, -2, 5, 0)  # emulates a Vector
+        point = Point(-2, 3, 1)
+        dest = point.add(vector)
+        assert isinstance(dest, tuple)
+        assert dest is not vector
+
+    def test_point_addition_raises_type_error(self):
+        p1 = Point(-2, 3, 1)
+        p2 = Point(3, -2, 5)
+        with pytest.raises(TypeError):
+            dest = p1.add(p2)  # Adding two Points does not make sense
+
+    def test_tuple_as_point_addition_raises_value_error(self):
+        point = Point(-2, 3, 1)
+        with pytest.raises(ValueError):
+            dest = point.add((3, -2, 5, 1))  # tuple that emulates a Point
+
+    def test_invalid_tuple_addition_raises_value_error(self):
+        point = Point(-2, 3, 1)
+        with pytest.raises(ValueError):
+            point.add((1, 2, 3, 4))  # tuple that is not a Point or a Vector
+
+    def test_invalid_type_argument_raises_type_error(self):
+        point = Point(-2, 3, 1)
+        with pytest.raises(TypeError):
+            point.add(7)  # one can not add a scalar to a point
